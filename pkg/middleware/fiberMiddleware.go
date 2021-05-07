1c6f20a87d3a74547b9384489af561a96d533e34
jadahbakar <dedy.styawan@gmail.com>
2021-05-07 14:36:27 +0700

    JWT middleware

--- pkg/middleware/fiberMiddleware.go
+++ pkg/middleware/fiberMiddleware.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -10,12 +13,18 @@ import (
 // FiberMiddleware provide Fiber's built-in middlewares.
 // See: https://docs.gofiber.io/api/middleware
 func FiberMiddleware(a *fiber.App) {
+	file, err := os.OpenFile("./log/fiber.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	// defer file.Close()
+
 	a.Use(
 		// Add CORS to each route.
 		cors.New(cors.Config{
 			Next:             nil,
 			AllowOrigins:     "*",
-			AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+			AllowMethods:     "GET,POST,PUT,DELETE",
 			AllowHeaders:     "Origin, Content-Type, Accept",
 			AllowCredentials: false,
 			ExposeHeaders:    "",
@@ -23,9 +32,10 @@ func FiberMiddleware(a *fiber.App) {
 		}),
 		// Add simple logger.
 		logger.New(logger.Config{
-			Format:     "${pid} ${locals:requestid} ${status} - ${method} ${path}\n",
-			TimeFormat: "02/Jan/2006-15:04:05",
+			Format:     "[${time}] ${pid} ${locals:requestid} ${status} - ${latency} ${method} ${path}\n",
+			TimeFormat: "2006/Jan/02 15:04:05",
 			TimeZone:   "Asia/Jakarta",
+			Output:     file,
 		}),
 		compress.New(compress.Config{
 			Level: compress.LevelBestSpeed,
