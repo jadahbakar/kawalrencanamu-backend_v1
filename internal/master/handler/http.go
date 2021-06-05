1979002be778f73fff36d3d483be9f7fe3130aaf
jadahbakar <dedy.styawan@gmail.com>
2021-06-05 09:42:40 +0700

    masih

--- internal/master/handler/http.go
+++ internal/master/handler/http.go
@@ -1,1 +1,27 @@
 package http
+
+import (
+	"net/http"
+
+	"github.com/jadahbakar/kawalrencanamu-backend/internal/master/entities"
+)
+
+type handler struct {
+	masterService entities.MasterService
+}
+
+type RedirectHandler interface {
+	Get(http.ResponseWriter)
+	Post(http.ResponseWriter)
+}
+
+func NewHandler(masterService entities.MasterService) RedirectHandler {
+	return &handler{masterService: masterService}
+}
+
+func (h *handler) Get(http.ResponseWriter) {
+
+}
+func (h *handler) Post(http.ResponseWriter) {
+
+}
